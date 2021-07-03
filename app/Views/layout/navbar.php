e0cc8156bca765bf182da9e2f2046622eef4bd81
rendifebrianto29 <rendifebrianto222@gmail.com>
2021-07-03 12:08:49 +0700

    mengubah warna navbar

--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -1,6 +1,6 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-        <a class="navbar-brand" href="#">Rendi</a>
+        <a class="navbar-brand" href="#">Komik ID</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
