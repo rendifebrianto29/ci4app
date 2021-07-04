1d0c8f4530a881dd5cf775ab1e7b801d4507126b
rendifebrianto29 <rendifebrianto222@gmail.com>
2021-07-04 15:00:08 +0700

    menambahkan modal form tambah data orang

--- app/Views/orang/index.php
+++ app/Views/orang/index.php
@@ -8,12 +8,56 @@
               <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
                   Tambah Data Orang
               </button>
+              <!-- Modal -->
+              <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                  <div class="modal-dialog">
+                      <div class="modal-content">
+                          <div class="modal-header">
+                              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                          </div>
+                          <div class="modal-body">
+                              <form action="/orang/save" method="post" enctype="multipart/form-data">
+                                  <?= csrf_field(); ?>
+                                  <div class="mb-3">
+                                      <label for="exampleInputEmail1" class="form-label">Nama</label>
+                                      <input type="text" name="nama[]" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                  </div>
+                                  <div class="mb-3">
+                                      <label for="exampleInputPassword1" class="form-label">Alamat</label>
+                                      <input type="text" name="alamat[]" class="form-control" id="exampleInputPassword1">
+                                  </div>
+                                  <div class="mb-3">
+                                      <label for="exampleInputEmail1" class="form-label">Nama</label>
+                                      <input type="text" name="nama[]" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                  </div>
+                                  <div class="mb-3">
+                                      <label for="exampleInputPassword1" class="form-label">Alamat</label>
+                                      <input type="text" name="alamat[]" class="form-control" id="exampleInputPassword1">
+                                  </div>
+
+                                  <button type="submit" class="btn btn-primary">Simpan</button>
+                              </form>
+                          </div>
+                          <div class="modal-footer">
+                              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                              <button type="button" class="btn btn-primary">Save changes</button>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+
               <form action="" method="post">
                   <div class="input-group mb-3">
                       <input type="text" class="form-control mt-3" placeholder="Masukkan keyword pencarian.." name="keyword">
                       <button class="btn btn-outline-secondary mt-3" type="submit" name="submit">Cari</button>
                   </div>
           </div>
+          <?php if (session()->getFlashdata('pesan')) : ?>
+              <div class="alert alert-success" role="alert">
+                  <?= session()->getFlashdata('pesan'); ?>
+              </div>
+          <?php endif; ?>
           </form>
       </div>
       <div class="row">
@@ -21,7 +65,7 @@
               <table class="table">
                   <thead>
                       <tr>
-                          <th scope="col">#</th>
+                          <th scope="col">No</th>
                           <th scope="col">Nama</th>
                           <th scope="col">Alamat</th>
                           <th scope="col">Aksi</th>
